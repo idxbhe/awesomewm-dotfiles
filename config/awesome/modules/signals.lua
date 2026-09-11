3d937168ff5260941c5579cfe1f9340ce12c1de8
idxbhe <75493634+idxbhe@users.noreply.github.com>
2026-09-11 14:33:59 +0700

    Fix titlebar minimize button: use release binding, remove debug print

--- config/awesome/modules/signals.lua
+++ config/awesome/modules/signals.lua
@@ -382,7 +382,7 @@ client.connect_signal("request::titlebars", function(c)
         box:connect_signal("mouse::enter", function() is_hover = true; update() end)
         box:connect_signal("mouse::leave", function() is_hover = false; update() end)
         box:buttons(gears.table.join(
-            awful.button({ }, 1, function()
+            awful.button({ }, 1, nil, function()
                 action(c)
             end)
         ))
@@ -395,7 +395,6 @@ client.connect_signal("request::titlebars", function(c)
         { { { align = "center", widget = awful.titlebar.widget.titlewidget(c), font = "Maple Mono NF Bold 9" }, buttons = buttons, layout = wibox.layout.flex.horizontal }, align = "center", valign = "center", widget = wibox.container.place },
         { tbbtn_icon(ic .. "minimize-normal.svg", ic .. "minimize-hover.svg", ic .. "nofocus.svg",
             function(c)
-                print("MINIMIZE BUTTON CLICKED")
                 c.minimized = true
             end, 16),
           tbbtn_icon(ic .. "maximize-normal.svg", ic .. "maximize-hover.svg", ic .. "nofocus.svg",
